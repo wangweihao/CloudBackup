c5433350d2dc78c47bb6421fecb5a944cf90507e
wwh <578867817@qq.com>
2015-12-26 00:56:42 +0800

    new server

---
+++ Server/WServer/workerServer/Server.cpp
@@ -0,0 +1,343 @@
+/*======================================================
+    > File Name: Server.cpp
+    > Author: wwh
+    > E-mail:  
+    > Other :  
+    > Created Time: 2015年12月24日 星期四 13时28分54秒
+ =======================================================*/
+
+#include "Server.h"
+
+
+WorkerServer::
+WorkerServer(std::string w_ip, int w_port, std::string b_ip, int b_port):ip(w_ip), port(w_port)
+{
+    if(!base)
+    {
+        perror("init event_base error");
+        exit(1);
+    }
+
+    /* init server socket */
+    bzero(&server, sizeof(server));    
+    server.sin_family = AF_INET;
+    server.sin_port = htons(port);
+    inet_pton(AF_INET, ip.c_str(), &server.sin_addr);
+    std::cout << "ip:" << ip << " port:" << port << std::endl;
+
+    listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if(listen_fd < 0)
+    {
+        perror("socket error");
+        exit(1);
+    }
+
+    /* set nonblocking */
+    evutil_make_socket_nonblocking(listen_fd);
+    
+    if(bind(listen_fd, (struct sockaddr*)&server, sizeof(server)) < 0)
+    {
+        perror("bind error");
+        exit(1);
+    }
+    
+    if(listen(listen_fd, MAX_BACKLOG) < 0)
+    {
+        perror("listen error");
+        exit(1);
+    }
+
+    /* new accept event */
+    accept_event = event_new(base, listen_fd, EV_READ | EV_PERSIST, accept_callback, (void*)base);
+
+    /* register accept event */
+    event_add(accept_event, NULL);
+}
+
+void 
+WorkerServer::run()
+{
+    std::cout << "loop" << std::endl;
+    /* start event loop */
+    event_base_dispatch(base);
+}
+
+WorkerServer::~WorkerServer()
+{
+
+}
+
+
+/* 上传文件
+ * 考虑两个用户同时上传一个文件怎么办，秒传？万一一个传到一半断了不传怎么办
+ * 考虑断点续传问题
+ * */
+bool 
+WorkerServer::handler_upload(int socket, std::string md5, unsigned long size, long offset, int threadNum)
+{
+    int filefd;
+    unsigned long file_size = 0;
+    unsigned long accumu_size = 0;
+    int pipefd[2];
+    ssize_t ret = 0;
+
+    /* file size */
+    file_size = size - offset;
+
+    /* open file*/
+    if((filefd = open(md5.c_str(), O_WRONLY | O_CREAT)) < 0)
+    {
+        perror("open file error");
+        return false;
+    }
+    /* offset not equal zero, breakpoint upload */
+    if(offset > 0)
+    {
+        if(lseek(filefd, SEEK_CUR, offset) < 0)
+        {
+            perror("lseek file error");
+            return false;
+        }
+    }
+
+    if(pipe(pipefd) < 0)
+    {
+        perror("create pipe error");
+        return false;
+    }
+    /* upload file */
+    while(1)
+    {
+        if((ret = splice(socket, NULL, pipefd[1], NULL, 32768, SPLICE_F_MORE | SPLICE_F_MOVE)) == -1){
+            perror("socket ==> pipefd[1] splice error");
+            break;
+        }else if(ret == 0)
+        {
+            break;
+        }
+        if((ret = splice(pipefd[0], NULL, filefd, NULL, 32768, SPLICE_F_MORE | SPLICE_F_MOVE)) == -1){
+            perror("pipe[0] ==> file splice error");
+            break;
+        }
+        accumu_size += ret;
+    }
+
+    if(accumu_size == file_size)
+    {
+        std::cout << "upload file success" << std::endl;
+    }else{
+        std::cout << "upload file error" << std::endl;
+        /* 断点续传，用户未上传成功，设置定时事件，
+         * 若用户在给定时间内没有断点续传，则触发定时事件，
+         * 删除服务器本地的文件 */
+        return false;
+    }
+
+    return true;
+}
+
+bool
+WorkerServer::handler_download(int socket, std::string md5, unsigned long size, long offset, int threadNum)
+{
+    int filefd;
+    unsigned long file_size = 0;
+    unsigned long accumu_size = 0;
+    ssize_t ret = 0;
+    struct stat file_state;
+
+    /* open file */
+    if((filefd = open(md5.c_str(), O_RDONLY)) < 0)
+    {
+        perror("file open error");
+        return false;
+    }
+
+    /* if file size equal zero, obtain file size */
+    if(size == 0)
+    {
+        if(stat(md5.c_str(), &file_state))
+        {
+            perror("obtain file size error");
+            return false;
+        }
+        size = file_state.st_size;   
+    }
+    
+    /* file size */
+    file_size = size - offset;
+
+    /* offset not equal zero, breakpoint download */
+    if(offset > 0)
+    {
+        if(lseek(filefd, SEEK_CUR, offset) < 0)
+        {
+            perror("lseek file error");
+            return false;
+        }
+    }
+
+    if((ret = sendfile(socket, filefd, &offset, file_size)) < 0)
+    {
+        perror("sendfile error");
+        return false;
+    }
+
+    if(ret == file_size)
+    {
+        std::cout << "sendfile success" << std::endl;
+    }else{
+        std::cout << "sendfile error" << std::endl;
+        /* 断点续传，用户未上传成功，设置定时事件，
+         * 若用户在给定时间内没有断点续传，则触发定时事件，
+         * 删除服务器本地的文件 */
+        return false;
+    }
+
+    return true;
+}
+
+
+/* 多线程下载
+ * 将文件均分成多个任务，
+ * 加入到任务队列中，
+ * 由多个线程处理*/
+bool 
+WorkerServer::multi_thread_download(int socket, std::string md5, unsigned long size, long offset, int threadNum)
+{
+    /* detect thread num */
+    for(int i = 0; i < threadNum; ++i)
+    {
+        handler_download(socket, md5, size, offset, 0);
+    }
+
+    return true;
+}
+
+void
+WorkerServer::accept_callback(evutil_socket_t listen_fd, short event, void* arg)
+{
+    std::cout << "收到连接" << std::endl;
+
+    struct event *read_event;
+    struct event *error_event;
+    struct sockaddr_storage client;
+    socklen_t length = sizeof(client);
+    int fd = accept(listen_fd, (struct sockaddr*)&client, &length);
+    if(fd < 0)
+    {
+        perror("accept error");
+        return;
+    }else{
+        struct bufferevent *bev;   
+        evutil_make_socket_nonblocking(fd);
+        read_event  = event_new(base, fd, EV_READ  | EV_PERSIST, read_callback,  (void*)base);
+        event_add(read_event, 0);
+    }
+}
+
+void 
+WorkerServer::read_callback(evutil_socket_t socket, short event, void *arg)
+{
+    std::cout << "read callback" << std::endl;
+    /* recv json and return ack then download */
+    char *buffer;
+    char jsonMsgLen[2];
+    Json::Reader reader;
+    Json::Value  value;
+
+    /* mark is a flag
+     * mark = 1 upload    (breakpoint_upload)
+     * mark = 2 download  (breakpoint_download)
+     * mark = 3 multithread download
+     * */
+    int mark = 0;
+    std::string md5;
+    unsigned long size = 0;
+    unsigned long offset = 0;
+    int threadNum = 0;
+
+
+
+    /* recv request (json)*/
+    /*
+     * Json:
+     * {
+     *      mark:1,     标识
+     *      md5:asd7qwebuuaweg12i3ehiqwueh, 
+     *      size:1900,  文件大小
+     *      offset:0,   偏移量  偏移量不为 0 则进行断点上传或下载
+     *      thread:1,   多线程下载
+     * }
+     * */
+    /* 先接收 2 字节 json 长度，接着接收 json 串 */
+    recv(socket, jsonMsgLen, 2, 0);
+    //short bufferLength = parse(jsonMsgLen);
+    short bufferlength = 10;
+        
+    buffer = (char*)malloc(sizeof(char) * bufferlength);
+    /* 即使设置 MSG_WAITALL 标识，不代表能接收 bufferlength 长度的数据包，需自己判定 */
+    if(recv(socket, buffer, bufferlength, MSG_WAITALL) == bufferlength)
+    {
+        printf("%s\n", buffer);
+    }else{
+        return;
+    }
+
+    /* parse json */
+    if(reader.parse(buffer, value))
+    {
+        if(!value["mark"].isNull())
+        {
+            mark = value["mark"].asInt();
+            md5 = value["md5"].asString();
+            size = value["size"].asLargestUInt();
+            offset = value["offset"].asLargestUInt();
+            threadNum = value["threadNum"].asInt();
+
+            if(detect_paramenter_correct(mark, md5, size, offset, threadNum) == false){
+                perror("detect parament correct error");
+                return;
+            }
+
+            /* create handle event */
+            Handler handle_event;
+
+            /* parse mark, handler event */
+            switch(mark){
+                case 1:
+                    handle_event = std::make_tuple(WorkerServer::handler_upload, socket, md5, size, offset, threadNum);
+                    threadpool.AddTask(std::move(handle_event));
+                    break;
+                case 2:
+                    handle_event = std::make_tuple(WorkerServer::handler_download, socket, md5, size, offset, threadNum);
+                    threadpool.AddTask(std::move(handle_event));
+                    break;
+                case 3:
+                    multi_thread_download(socket, md5, size, offset, threadNum);
+                    handle_event = std::make_tuple(WorkerServer::multi_thread_download, socket, md5, size, offset, threadNum);
+                    threadpool.AddTask(std::move(handle_event));
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
+
+void 
+WorkerServer::error_callback(evutil_socket_t socket, short event, void *arg)
+{
+    std::cout << "error_callaback" << std::endl;
+    sleep(1);
+}
+
+bool detect_paramenter_correct(int mark, std::string md5, unsigned long size, unsigned long offset, int threadNum)
+{
+    if((mark > 0 && mark < 4) && (!md5.empty()) && (size >= 0) && (offset >=0) && (threadNum >= 0))
+    {
+        return true;
+    }else{
+        return false;
+    }
+}
+
---
+++ Server/WServer/workerServer/Server.h
@@ -0,0 +1,93 @@
+/*======================================================
+    > File Name: Server.h
+    > Author: wwh
+    > E-mail:  
+    > Other :  
+    > Created Time: 2015年12月23日 星期三 13时57分56秒
+ =======================================================*/
+
+#ifndef _SERVER_H_
+#define _SERVER_H_
+
+
+/* C++ header */
+#include <iostream>
+#include <string>
+
+/* C   header*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/stat.h>
+#include <sys/sendfile.h>
+#include <json/json.h>
+
+
+/* libevent header*/
+#include <event2/event.h>
+#include <event2/buffer.h>
+#include <event2/bufferevent.h>
+
+/* my head */
+#include "ThreadPool.h"
+
+
+#define MAX_BACKLOG 1024
+#define JSONMSG_BUF 256
+
+
+class WorkerServer{
+    public:
+        /* init function */
+        WorkerServer(std::string w_ip, int w_port, std::string b_ip, int b_port);
+
+        /* realise resourse */
+        ~WorkerServer();
+
+    public:
+        void run();
+
+    public:
+        /* file upload    (breakpoint upload)*/
+        static bool handler_upload(int socket, std::string md5, unsigned long size, long offset, int threadNum);
+        /* file download  (breakpoint download)*/
+        static bool handler_download(int socket, std::string md5, unsigned long size, long offset, int threadNum);
+        /* multi thread download */
+        static bool multi_thread_download(int socket, std::string md5, unsigned long size, long offset, int threadNum);
+
+    private:
+        /* event callback */
+        static void error_callback(evutil_socket_t socket, short event, void* arg);
+        static void read_callback(evutil_socket_t socket, short event, void* arg);
+        static void accept_callback(evutil_socket_t listen_fd, short event, void* arg);
+
+    private:    
+        /* WorkerServer ip and port */
+        std::string ip;
+        int port;
+
+        /* server info*/
+        struct sockaddr_in server;
+
+        /* listen socket */
+        evutil_socket_t listen_fd;
+        
+        /* libevent accept event */
+        struct event *accept_event;
+
+        static struct event_base *base;
+
+    private:
+        /* help */
+        static ThreadPool threadpool;
+};
+
+bool detect_paramenter_correct(int, std::string, unsigned long, unsigned long, int);
+
+#endif
